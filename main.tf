206261629b6473a966acf6c59bb06f0cf63865e5
unknown <arun.mohapatra@yahoo.com>
2022-07-14 00:20:19 +0530

    add s3

--- main.tf
+++ main.tf
@@ -33,6 +33,10 @@ resource "aws_instance" "my_instance" {
     provider = aws.us-east-1
 }
 
+resource "aws_s3_bucket" "mybycket" {
+  bucket = "arun-12071980"
+}
+
 output "instance_public_ip" {
   value = aws_instance.my_instance.public_ip
 }
